a92e978de7e5d65772baa60f3516a9bde486fd45
David Garcia <albarranco2@gmail.com>
2023-09-13 21:04:20 +0200

    seccciones ok

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ console.log(card)
 for (let i=0 ; i < card.length; i++){
   card[i].addEventListener( 'click',function() {
 	console.log("card pulsada");
-    card[i].classList.toggle('is-flipped');
+  card[i].classList.toggle('is-flipped');
   });
 }
