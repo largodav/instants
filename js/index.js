19f852d7fd7b2e49be558c41c2cd58bfcf9f3f0d
David Garcia <albarranco2@gmail.com>
2023-09-15 12:40:46 +0200

    AHORA SI...

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const card = document.querySelectorAll(".card");
 
-console.log(card)
+// console.log(card)
 for (let i=0 ; i < card.length; i++){
   card[i].addEventListener( 'click',function() {
-	console.log("card pulsada");
+	// console.log("card pulsada");
   card[i].classList.toggle('is-flipped');
   });
 }
