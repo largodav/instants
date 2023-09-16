ca0b4a842ef3b7c54df92482e21eff8c0adfff20
David Garcia <albarranco2@gmail.com>
2023-09-16 10:49:13 +0200

    tenemos la seccion de enlaces..

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 const card = document.querySelectorAll(".card");
+const card_frontal = document.querySelectorAll(".card__face--front");
+const card_back = document.querySelectorAll(".card__face--back");
 
 // console.log(card)
 for (let i=0 ; i < card.length; i++){
   card[i].addEventListener( 'click',function() {
 	// console.log("card pulsada");
   card[i].classList.toggle('is-flipped');
+  card_frontal[i].classList.toggle('ocultar');
+  card_back[i].classList.remove('ocultar');
+  card_back[i].classList.toggle('mostrar');
   });
 }
