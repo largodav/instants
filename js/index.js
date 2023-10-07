2053cabf7ad8f2edd591e52ed8438173d2f56661
David Garcia <albarranco2@gmail.com>
2023-10-07 11:19:46 +0200

    refactorizando codigo de los loop d eoamgenes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,28 @@
 
+//Establecemos el numero de imagenes para los bucles
 const num_imagenes = 8;
+//contador actualziado
 var i = 0;
+var carrusel_instants = document.querySelectorAll(".imagens_instants");
+var carrusel_play = document.querySelectorAll(".imagens_play");
+var carrusel_dioramas = document.querySelectorAll(".imagens_dioramas");
+var instants = "";
+var play ="";
+var dioramas= "";
 
-function imagenes(i){
-  var carrusel_instants = document.querySelectorAll(".imagens_instants");
-  console.log(i);
- if (i > num_imagenes){
-  i = 0;
-  
- }else{
-  carrusel_instants[0].src =  "./img/instants/imagenes_carrusel/instants_" + i + ".jpg";
-}
+/** FUNCIONES PARA EL LOOP DE IMAGENES, ESTO HAY QUE REFACTORIZAR */
 
 
+//Para refactorizar...
+async function imagenes(i,loop,contenido){
+  // var carrusel_instants = document.querySelectorAll(".imagens_instants");
+  //console.log(i);
+    if (i > num_imagenes){
+      i = 0;
+      
+    }else{
+      loop[0].src =  "./img/"+ contenido + "/imagenes_carrusel/" + contenido +"_" + i + ".jpg";
+    }
 }
 setInterval(function()
 {
@@ -20,8 +30,13 @@ setInterval(function()
     i=0;
   }
   i++;
-  imagenes(i);
+  imagenes(i,carrusel_instants,"instants");
+  imagenes(i,carrusel_dioramas,"dioramas");
+  imagenes(i,carrusel_play,"play");
 
-},2500);
+},Math.random() *(2500 - 1000 ) + 1000);
 
+
+
+/** FIN DEL LOOP DE IMAGENES */
 
